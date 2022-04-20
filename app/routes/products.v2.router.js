0101400e39fd964396cca5a67f7e254be67ee930
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-04-20 12:06:41 -0500

    Ejemplo de endpoints PATCH y DELETE

--- app/routes/products.v2.router.js
+++ app/routes/products.v2.router.js
@@ -44,6 +44,24 @@ router.post('/', (req, res) => {
   res.json({
     message: 'created',
     data: body
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'updated',
+    data: body,
+    id
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id
   })
 });
 

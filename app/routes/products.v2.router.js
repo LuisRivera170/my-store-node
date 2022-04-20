66ac46f122b75d9bf71c65c993b40f35d06968d1
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-04-20 14:05:22 -0500

    Uso de try-catch para manejo de errores

--- app/routes/products.v2.router.js
+++ app/routes/products.v2.router.js
@@ -8,7 +8,8 @@ const productService = new ProductService();
 router.get('', async (req, res) => {
   const { limit } = req.query;
   const products = await productService.find(+limit);
-  res.json(products);
+  res
+    .json(products);
 });
 
 /* Nota: Para evitar conflictos, los enpoints con rutas dinámicas
@@ -16,7 +17,8 @@ router.get('', async (req, res) => {
 */
 router.get('/filter', async (_, res) => {
   const randomProduct = await productService.filter();
-  res.json(randomProduct);
+  res
+    .json(randomProduct);
 });
 
 router.get('/:id', async (req, res) => {
@@ -37,19 +39,38 @@ router.post('/', async (req, res) => {
 });
 
 router.patch('/:id', async (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const productUpdated = await productService.update(+id, body);
-  res.json(productUpdated);
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const productUpdated = await productService.update(+id, body);
+    res
+      .status(200)
+      .json(productUpdated);
+  } catch(error) {
+    res
+      .status(404)
+      .json({
+        message: error.message
+      });
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const deletedProduct = await productService.delete(+id);
-  res.json({
-    message: 'deleted',
-    deletedProduct
-  })
+  try {
+    const { id } = req.params;
+    const deletedProduct = await productService.delete(+id);
+    res
+      .json({
+        message: 'deleted',
+        deletedProduct
+      });
+  } catch(error) {
+    res
+      .status(404)
+      .json({
+        message: error.message
+      });
+  }
 });
 
 module.exports = router;

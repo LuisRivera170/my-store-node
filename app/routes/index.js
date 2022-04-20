da8e8ab0ec85443b9a238667a96bfa50c8ea70f1
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-04-20 11:33:15 -0500

    Mejorando manejo de versionamiento de api

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,20 @@
+const express = require('express');
+
 const categoryV1Router = require('./categories.v1.router');
 const productV1Router = require('./products.v1.router');
 const productV2Router = require('./products.v2.router');
 const userV1Router = require('./users.v1.router');
 
 function routerApi(app) {
-  app.use('/api/v1/products', productV1Router);
-  app.use('/api/v2/products', productV2Router);
-  app.use('/api/v1/categories', categoryV1Router);
-  app.use('/api/v1/users', userV1Router);
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productV1Router);
+  router.use('/categories', categoryV1Router);
+  router.use('/users', userV1Router);
+
+  const routerv2 = express.Router();
+  app.use('/api/v2', routerv2);
+  routerv2.use('/products', productV2Router);
 }
 
 module.exports = routerApi;

bad125acb50b69463ffac926e571496756d61ff0
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-04-20 12:13:06 -0500

    Manejo de códigos de estatus HTTP

--- app/routes/products.v2.router.js
+++ app/routes/products.v2.router.js
@@ -29,22 +29,32 @@ router.get('/filter', (_, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json(
-    {
-      id,
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
-    }
-  );
+  if (id === '999') {
+    res
+      .status(404)
+      .json({
+        message: 'Not found'
+      });
+  } else {
+    res
+      .status(200)
+      .json({
+        id,
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.imageUrl()
+      });
+  }
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
-    message: 'created',
-    data: body
-  });
+  res
+    .status(201)
+    .json({
+      message: 'created',
+      data: body
+    });
 });
 
 router.patch('/:id', (req, res) => {
